83652b2da1e4487ba5722c6a92127e3b8b8448c5
Zain Ul Basit <zainulbasit486@gmail.com>
2024-01-27 14:19:42 +0500

    DB connected

--- app/api/user/route.js
+++ app/api/user/route.js
@@ -1,8 +1,17 @@
-import { connectionSrt } from "@/lib/db";
+import connectDB from "@/lib/db";
 import { Users } from "@/lib/models/users";
-import mongoose from "mongoose";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  return NextResponse.json({ result: true });
+  try {
+    await connectDB();
+    return NextResponse.json({ success: true });
+  } catch (err) {
+    return NextResponse.json({
+      success: false,
+      error: {
+        msg: err,
+      },
+    });
+  }
 }
